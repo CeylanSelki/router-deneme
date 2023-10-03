55e65554dc6a04b3933faa96589524b7162c1a47
[Ceylan] <[ceylancengiz1@gmail.com]>
2023-10-03 23:58:47 +0300

    ürünler için css eklendi ve app.jsx revize

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './app.css'
 
 function App() {
     const [post, setPost] = useState ([]);
@@ -9,17 +10,28 @@ function App() {
 
     },[])
     console.log(post)
-  
+
     return (
-        <div>
-            {post.map(a => (
-                <div key={a.id}>
-                    <div>{a.title}</div>
-                    <div>{a.body}</div>
-                </div>
-            ))}
+        <div className="post-container">
+          {post.map(a => (
+            <div className="post-item" key={a.id}>
+              <div>{a.title}</div>
+              <div>{a.body}</div>
+            </div>
+          ))}
         </div>
-    )
+      );
+  
+    // return (
+    //     <div>
+    //         {post.map(a => (
+    //             <div key={a.id}>
+    //                 <div>{a.title}</div>
+    //                 <div>{a.body}</div>
+    //             </div>
+    //         ))}
+    //     </div>
+    // )
 }
 
 export default App;
