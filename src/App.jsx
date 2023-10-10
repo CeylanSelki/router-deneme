447ae6cf9f8b3e101496b4f212f75f78fdebb8db
[Ceylan] <[ceylancengiz1@gmail.com]>
2023-10-10 13:55:16 +0300

    css düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,84 @@
-import { useEffect, useState } from "react";
-import './app.css'
+// import { useEffect, useState } from "react";
+// import './app.css'
+// // import data from '../data/data.json'; 
+
+
+
+// function App() {
+//     const [post, setPost] = useState ([]);
+
+//     useEffect(()=> {
+//         fetch ('https://jsonplaceholder.typicode.com/posts').then(x => x.json())
+//             .then(x => setPost(x));
+
+//     },[])
+//     console.log(post)
+
+//     return (
+//         <div className="post-container">
+//           {post.map(a => (
+//             <div className="post-item" key={a.id}>
+//               <div>{a.title}</div>
+//               <div>{a.body}</div>
+//             </div>
+//           ))}
+//         </div>
+//       );
+  
+//     // return (
+//     //     <div>
+//     //         {post.map(a => (
+//     //             <div key={a.id}>
+//     //                 <div>{a.title}</div>
+//     //                 <div>{a.body}</div>
+//     //             </div>
+//     //         ))}
+//     //     </div>
+//     // )
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from "react";
+import './app.css';
 
 function App() {
-    const [post, setPost] = useState ([]);
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
 
-    useEffect(()=> {
-        fetch ('https://jsonplaceholder.typicode.com/posts').then(x => x.json())
-            .then(x => setPost(x));
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Veri alınamadı');
+                }
+                return response.json();
+            })
+            .then(data => setPosts(data))
+            .catch(err => setError(err.message));
+    }, []);
 
-    },[])
-    console.log(post)
+    if (error) {
+        return <div>Hata: {error}</div>;
+    }
 
     return (
         <div className="post-container">
-          {post.map(a => (
-            <div className="post-item" key={a.id}>
-              <div>{a.title}</div>
-              <div>{a.body}</div>
+            <div className="post-header">
+                <img src="https://images.pexels.com/photos/3965545/pexels-photo-3965545.jpeg" alt="" />
             </div>
-          ))}
+            {posts.map((post) => (
+                <div className="post-item" key={post.id}>
+                    { (
+                        <>
+                            <div className="post-title">{post.title}</div>
+                            <div className="post-body">{post.body}</div>
+                        </>
+                    )}
+                </div>
+            ))}
         </div>
-      );
-  
-    // return (
-    //     <div>
-    //         {post.map(a => (
-    //             <div key={a.id}>
-    //                 <div>{a.title}</div>
-    //                 <div>{a.body}</div>
-    //             </div>
-    //         ))}
-    //     </div>
-    // )
+    );
 }
 
 export default App;
